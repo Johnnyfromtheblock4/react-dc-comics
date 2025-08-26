899ea22d91f0483ff07a8e6fa90d324032e720f2
Johnnyfromtheblock4 <simo.visco4@gmail.com>
2025-08-27 01:25:32 +0200

    [UPD] impostazione navbar

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -7,40 +7,7 @@ const Header = () => {
               <img src="./img/dc-logo.png" alt="DC Logo" />
             </div>
           </div>
-          <div className="col-75">
-            <ul className="menu">
-              <li>
-                <a href="#">CHARACTERS</a>
-              </li>
-              <li className="active">
-                <a href="#">COMICS</a>
-              </li>
-              <li>
-                <a href="#">MOVIES</a>
-              </li>
-              <li>
-                <a href="#">TV</a>
-              </li>
-              <li>
-                <a href="#">GAMES</a>
-              </li>
-              <li>
-                <a href="#">COLLECTIBLES</a>
-              </li>
-              <li>
-                <a href="#">VIDEOS</a>
-              </li>
-              <li>
-                <a href="#">FANS</a>
-              </li>
-              <li>
-                <a href="#">NEWS</a>
-              </li>
-              <li>
-                <a href="#">SHOP</a>
-              </li>
-            </ul>
-          </div>
+          
         </div>
       </div>
   );
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,81 @@
+const Navbar = () => {
+  const links = [
+    {
+      id: 1,
+      label: "CHARACTERS",
+      href: "#",
+      current: false,
+    },
+    {
+      id: 2,
+      label: "COMICS",
+      href: "#",
+      current: true,
+    },
+    {
+      id: 3,
+      label: "MOVIES",
+      href: "#",
+      current: false,
+    },
+    {
+      id: 4,
+      label: "TV",
+      href: "#",
+      current: false,
+    },
+    {
+      id: 5,
+      label: "GAMES",
+      href: "#",
+      current: false,
+    },
+    {
+      id: 6,
+      label: "COLLECTIBLES",
+      href: "#",
+      current: false,
+    },
+    {
+      id: 7,
+      label: "VIDEOS",
+      href: "#",
+      current: false,
+    },
+    {
+      id: 8,
+      label: "FANS",
+      href: "#",
+      current: false,
+    },
+    {
+      id: 9,
+      label: "NEWS",
+      href: "#",
+      current: false,
+    },
+    {
+      id: 10,
+      label: "SHOP",
+      href: "#",
+      current: false,
+    },
+  ];
+  return (
+    <div className="col-75">
+      <ul className="menu">
+        {links.map((link) => {
+          // metodo deconstractor
+          const { id, label, href, current } = link;
+          return (
+            <li key={id}>
+              <a href={href} className={current === true ? "active" : ""}>
+                {label}
+              </a>
+            </li>
+          );
+        })}
+      </ul>
+    </div>
+  );
+};
