0d0da2cd390e70224b54b3d9da209dad89ed186d
Johnnyfromtheblock4 <simo.visco4@gmail.com>
2025-08-27 01:51:43 +0200

    [UPD] importo l'array di oggetti da comics.js

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,13 +1,17 @@
+import comics from "../assets/comics";
+
 const Main = () => {
   return (
     <main>
       <div className="container">
         <div className="row">
-          <div className="col-100">
-            <span>
-              <i class="fa-solid fa-arrow-right"></i>Content goes here
-              <i class="fa-solid fa-arrow-left"></i>
-            </span>
+          <div className="col">
+            {comics.map((comic) => (
+              <div key={comic.id}>
+                <img src={comic.thumb} alt={comic.title} />
+                <h3>{comic.title}</h3>
+              </div>
+            ))}
           </div>
         </div>
       </div>
