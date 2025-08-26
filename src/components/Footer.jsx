814f1d58f1b7dca2823c738c7c3533d3a0a6f1f3
Johnnyfromtheblock4 <simo.visco4@gmail.com>
2025-08-26 22:23:37 +0200

    [UPD] middle-footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -8,7 +8,7 @@ const Footer = () => {
               <div className="icons-list">
                 <div>
                   <img
-                    src="./img/buy-comics-digital-comics.png"
+                    src="../img/buy-comics-digital-comics.png"
                     className="icon"
                     alt="Digital Comics"
                   />
@@ -51,7 +51,58 @@ const Footer = () => {
           </div>
         </div>
       </div>
-      <div className="middle-footer"></div>
+      <div className="middle-footer">
+        <div className="container">
+          <div className="row">
+            <div className="col-50 menu-footer">
+              <div className="col-33">
+                <h2>DC COMICS</h2>
+                <ul>
+                  <li>Characters</li>
+                  <li>Comics</li>
+                  <li>Movies</li>
+                  <li>TV</li>
+                  <li>Games</li>
+                  <li>Videos</li>
+                  <li>News</li>
+                </ul>
+                <h2>SHOP</h2>
+                <ul>
+                  <li>Shop DC</li>
+                  <li>Shop DC Collectibles</li>
+                </ul>
+              </div>
+              <div className="col-33">
+                <h2>DC</h2>
+                <ul>
+                  <li>Therms of Use</li>
+                  <li>Privacy policy (New)</li>
+                  <li>Ad Choices</li>
+                  <li>Advertising</li>
+                  <li>Jobs</li>
+                  <li>Subscription</li>
+                  <li>Talent Workshop</li>
+                  <li>CPSC Certificates</li>
+                  <li>Ratings</li>
+                  <li>Shop Help</li>
+                  <li>Contact Us</li>
+                </ul>
+              </div>
+              <div className="col-33">
+                <h2>SITES</h2>
+                <ul>
+                  <li>DC</li>
+                  <li>MAD Magazine</li>
+                  <li>DC Kids</li>
+                  <li>DC Universe</li>
+                  <li>DC Power Visa</li>
+                </ul>
+              </div>
+            </div>
+            <div className="col-50 background-dc"></div>
+          </div>
+        </div>
+      </div>
       <div className="bottom-footer"></div>
     </footer>
   );
