c10711d6947cd32b4ad2d75885ce67e68d6bece7
Johnnyfromtheblock4 <simo.visco4@gmail.com>
2025-08-26 20:26:50 +0200

    [UPD] main

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,9 +1,18 @@
 const Main = () => {
   return (
-    <div>
-      
-    </div>
-  )
-}
+    <main>
+      <div className="container">
+        <div className="row">
+          <div className="col-100">
+            <span>
+              <i class="fa-solid fa-arrow-right"></i>Content goes here
+              <i class="fa-solid fa-arrow-left"></i>
+            </span>
+          </div>
+        </div>
+      </div>
+    </main>
+  );
+};
 
-export default Main
+export default Main;
