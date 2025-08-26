c6dd978100198e86f3b888e5cf8880992ef2abb0
Johnnyfromtheblock4 <simo.visco4@gmail.com>
2025-08-26 21:43:25 +0200

    [UPD] top-footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,60 @@
 const Footer = () => {
   return (
-    <div>
-      
-    </div>
-  )
-}
+    <footer>
+      <div className="top-footer">
+        <div className="container">
+          <div className="row">
+            <div className="col-100">
+              <div className="icons-list">
+                <div>
+                  <img
+                    src="./img/buy-comics-digital-comics.png"
+                    className="icon"
+                    alt="Digital Comics"
+                  />
+                  <span>DIGITAL COMICS</span>
+                </div>
+                <div>
+                  <img
+                    src="./img/buy-comics-merchandise.png"
+                    className="icon"
+                    alt="Merchandise"
+                  />
+                  <span>DC MERCHANDISE</span>
+                </div>
+                <div>
+                  <img
+                    src="./img/buy-comics-subscriptions.png"
+                    className="icon"
+                    alt="Subscription"
+                  />
+                  <span>SUBSCRIPTION</span>
+                </div>
+                <div>
+                  <img
+                    src="./img/buy-comics-shop-locator.png"
+                    className="icon"
+                    alt="Shop Locator"
+                  />
+                  <span>SHOP LOCATOR</span>
+                </div>
+                <div>
+                  <img
+                    src="./img/buy-dc-power-visa.svg"
+                    className="icon"
+                    alt="Power Visa"
+                  />
+                  <span>DC POWER VISA</span>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="middle-footer"></div>
+      <div className="bottom-footer"></div>
+    </footer>
+  );
+};
 
-export default Footer
+export default Footer;
