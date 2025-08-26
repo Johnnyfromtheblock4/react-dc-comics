0a382458147c9a5b95cb253b3eb53cd6dcf9ea7f
Johnnyfromtheblock4 <simo.visco4@gmail.com>
2025-08-26 19:31:07 +0200

    [UPD] creazione/importazione file .jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,16 @@
-function App() {}
+// importo header-main-footer
+import Header from ".components/Header";
+import Main from ".components/Main";
+import Footer from ".components/Footer";
+
+function App() {
+  return (
+    <>
+      <Header />
+      <Main />
+      <Footer />
+    </>
+  );
+}
 
 export default App;
