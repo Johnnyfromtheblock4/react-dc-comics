7b62865490e9ce6d20ebac70e8a51b8c0ed07bd9
Johnnyfromtheblock4 <simo.visco4@gmail.com>
2025-08-27 01:59:14 +0200

    [UPD] jumbotron

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -3,9 +3,10 @@ import comics from "../assets/comics";
 const Main = () => {
   return (
     <main>
+      <img src="../public/img/jumbotron.jpg" alt="Jumbotron" className="jumbotron" />
       <div className="container">
         <div className="row">
-          <div className="col">
+          <div className="col lista-fumetti">
             {comics.map((comic) => (
               <div key={comic.id}>
                 <img src={comic.thumb} alt={comic.title} />
