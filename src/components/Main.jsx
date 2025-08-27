b3c0a239a93bd078f920d62a4d26a12ee5bcbb15
Johnnyfromtheblock4 <simo.visco4@gmail.com>
2025-08-27 10:05:43 +0200

    [UPD] correzione esercizio

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,29 +1,7 @@
-import comics from "../assets/comics";
+import ComicsList from "../components/ComicsList";
 
 const Main = () => {
-  return (
-    <main>
-      <img
-        src="../public/img/jumbotron.jpg"
-        alt="Jumbotron"
-        className="jumbotron"
-      />
-      <div className="container">
-        <div className="row">
-          <h2 className="main-title">CURRENT SERIES</h2>
-          <div className="col lista-fumetti">
-            {comics.map((comic) => (
-              <div key={comic.id}>
-                <img src={comic.thumb} alt={comic.title} />
-                <h3>{comic.series}</h3>
-              </div>
-            ))}
-            <button className="load-more">Load More</button>
-          </div>
-        </div>
-      </div>
-    </main>
-  );
+  return <ComicsList />;
 };
 
 export default Main;
